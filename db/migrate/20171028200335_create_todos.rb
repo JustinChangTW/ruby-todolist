a2b27ee0839a822bbec57414b18f6683cd8bffd5
justin <god.chang.tw@gmail.com>
2017-10-29 04:11:54 +0800

    建立資料表與資料欄位

--- db/migrate/20171028200335_create_todos.rb
+++ db/migrate/20171028200335_create_todos.rb
@@ -1,7 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[5.1]
   def change
     create_table :todos do |t|
-
+      t.string :name # 名稱
+      t.date :due_date # 必需完成時間
+      t.string :note # 其它說明
       t.timestamps
     end
   end
