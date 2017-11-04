8948d3a15d12072d315f638c2b9184f28bb8c159
justin <god.chang.tw@gmail.com>
2017-11-05 03:20:56 +0800

    在model>todo.rb中加入完成日是否大於今日辨斷，若大於則將View中不顯示刪除功態

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,17 @@
 class Todo < ApplicationRecord
+    def is_due_date_over
+
+      date = Time.new.to_s
+      year, month, day = date.split(/-/)
+
+      today=year + "-" + month + "-" + day
+      date = Time.new.to_s
+      due_date_s = due_date.to_s
+      if (due_date_s <= today) 
+        true
+      else
+        false
+      end
+    end
+    
 end
