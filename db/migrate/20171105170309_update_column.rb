aae8f25db839f07e9797e17505fe602c61baabac
justin <god.chang.tw@gmail.com>
2017-11-06 01:06:55 +0800

    增加欄位，記錄已完成項目

---
+++ db/migrate/20171105170309_update_column.rb
@@ -0,0 +1,5 @@
+class UpdateColumn < ActiveRecord::Migration[5.1]
+  def changep
+    add_column :todos, :doen,:boolone
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028200335) do
+ActiveRecord::Schema.define(version: 20171105170309) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
